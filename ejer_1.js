b92c22c95e0189125a11dee9ced70f370dfa7e41
celialopez1503 <celialopez1503@gmail.com>
2021-02-16 16:02:09 +0100

    m ñkj

---
+++ ejer_1.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const pregunta = prompt('¿en que año estamos?');
+
+if (pregunta%4 == 0){
+    console.log('Se encuentra en un año bisiesto');
+} else if (pregunta%4 !=0){
+    console.log('no se encuentra en un año bisiesto')   
+}; 
+
+const question = prompt ('en que año estamos')
